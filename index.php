790b6949343200859e906bda1a61092df0e522e0
handy handoko <phan.handy.handoko@gmail.com>
2017-08-24 16:56:51 +0800

    tampilkan id provinsi ketika dropdown berubah

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'koneksi.php';
 
-//buat perintah sql untuk query data provinsi
+//buat perintah sql untuk query data provinsi. ambil kolom id, dan name dari tabel provinces
 $sql = "SELECT id, name FROM provinces";
 $stmt = $dbCon->prepare($sql);
 $stmt->execute();
@@ -19,9 +19,12 @@ $data_provinsi = $stmt->fetchAll();
 		Provinsi
 			<select id="provinsi">
 
-				<?php //isi data dari variabel provinsi ke dalam dropdown.
+				<?php
+				//isi data dari variabel provinsi ke dalam dropdown. isi dari field id dimasukkan ke dalam attribute value. sementara isi field name dimasukkan sebagai text
 				foreach($data_provinsi as $provinsi) {?>
-					<option value="<?php echo $provinsi['id']; ?>"><?php echo $provinsi['name']; ?></option>
+					<option value="<?php echo $provinsi['id']; ?>">
+						<?php echo $provinsi['name']; ?>
+					</option>
 				<?php } ?>
 			
 			</select>
@@ -30,5 +33,17 @@ $data_provinsi = $stmt->fetchAll();
 		<select id="kabupatenkota">
 			<option>Silahkan pilih kabupaten/kota</option>
 		</select>
+
+		<script type="text/javascript">
+			$(document).ready(function() {
+
+				//function ini akan aktif ketika dropdown dengan id provinsi berubah
+				$('#provinsi').change(function() {
+
+					//perintah ini akan menampilkan alert berisi val dari dropdown provinsi. dalam hal ini, karena val dari dropdown adalah id provinsi, maka alert akan menampilkan id provinsi
+					alert($('#provinsi').val());
+				});
+			});
+		</script>
 	</body>
 </html>

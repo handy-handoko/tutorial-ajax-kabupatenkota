64819037a3b322ef8f748398ae7264261d9d3b00
handy handoko <phan.handy.handoko@gmail.com>
2017-08-24 14:52:03 +0800

    tampilkan semua nama kabupatenkota
    get semua data kabupaten kota menggunakan perintah sql. lalu tampilkan dengan menggunakan perintah print_r

--- getkabupatenkota.php
+++ getkabupatenkota.php
@@ -1,3 +1,11 @@
 <?php 
 //panggil file koneksi.php agar bisa terhubung ke database
 include 'koneksi.php';
+
+//query semua data dari regencies(kabupaten/kota)
+$sql = "SELECT * FROM regencies";
+$stmt = $dbCon->prepare($sql);
+$stmt->execute();
+
+//lalu tampilkan data menggunakan perintah fetchAll
+print_r($stmt->fetchAll());

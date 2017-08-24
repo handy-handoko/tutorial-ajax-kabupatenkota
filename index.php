9a22ba58c68f8616b9037515a2c8d83cb63d4665
handy handoko <phan.handy.handoko@gmail.com>
2017-08-24 15:35:31 +0800

    tambah dropdown provinsi dan kabkota

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	</head>
 	<body>
-		
+		Provinsi
+		<select id="provinsi">
+			<option>Silahkan pilih provinsi</option>
+		</select>
+		<br/>
+		Kabupaten/Kota
+		<select id="kabupatenkota">
+			<option>Silahkan pilih kabupaten/kota</option>
+		</select>
 	</body>
 </html>

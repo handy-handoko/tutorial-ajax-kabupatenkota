32b698d8c5033f32a72ab3b45f153eec4f3c465c
handy handoko <phan.handy.handoko@gmail.com>
2017-08-24 15:05:02 +0800

    get data provinsi

--- getkabupatenkota.php
+++ getkabupatenkota.php
@@ -2,6 +2,9 @@
 //panggil file koneksi.php agar bisa terhubung ke database
 include 'koneksi.php';
 
+//tambahkan variabel provinsi. variabel provinsi akan mendapat data dari parameter provinsi yang dimasukkan di browser.
+$provinsi =  $_GET['provinsi'];
+
 //query semua data dari regencies(kabupaten/kota)
 $sql = "SELECT * FROM regencies";
 $stmt = $dbCon->prepare($sql);
